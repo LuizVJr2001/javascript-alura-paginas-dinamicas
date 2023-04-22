669cec8cb2f40b6127fef46e32a6976f68fc98e8
LuizVJr2001 <you@example.com>
2023-04-22 14:43:25 -0300

    projeto finalizado - aula 5

--- JavascriptAlura/aluramidi-curso-arquivos-iniciais/main.js
+++ JavascriptAlura/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,9 +1,40 @@
-function tocaSomPom() {  
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementAudio) {
+  elemento = document.querySelector(idElementAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+  else {
+    console.log('Elemento não encontrado ou seletor inválido');
+  }
 }
 
-function tocaSomClap() {  
-  document.querySelector('#som_tecla_clap').play();
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+for (i = 0; i < listaDeTeclas.length; i++) {
+  //cada elemento da lista de teclas
+  const tecla = listaDeTeclas[i]
+
+  //pega a classe dos elementos teclas
+  const instrumento = tecla.classList[1];
+
+  //template string (usa a classe das teclas para compor o id dos audios)
+  const idAudio = `#som_${instrumento}`;
+
+  //função anonima JS
+  tecla.onclick = function () {
+    tocaSom(idAudio)
+  }
+
+
+  //uso de evento em funções atreladas a evento
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa')
+  }
+}
